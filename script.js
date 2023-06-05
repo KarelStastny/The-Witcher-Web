2eeda1707963f85db18c62802b0fe522d90d9f29
Karel Šťastný <karell29@icloud.com>
2023-06-05 11:10:11 +0200

    přidán automaticky vypsiující se text nadpisu

--- script.js
+++ script.js
@@ -3,9 +3,35 @@
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
 
-
+// Po kliknutí na hamburger jse prohodí classy 
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
 
+
+
+
+
+// Auto Write text heding
+let hedding = document.querySelector(".heddingText")
+let hedding2 = document.querySelector(".heddingText2")
+let text = "The Witcher "
+let text2 = "Fan club for Life"
+let idFirstLetter = 1
+let speed = 500 / 8
+
+
+printText = () => {
+    // Rozdělím text na písmena
+    hedding.textContent = text.slice(0, idFirstLetter)
+    hedding2.textContent = text2.slice(0, idFirstLetter)
+    
+    // přidá navíším letter o 1 aby se vzalo další písmeno
+    idFirstLetter = idFirstLetter + 1
+
+    // spoustí funci se spožděním
+    setTimeout(printText, speed)
+}
+
+printText()
