69a0304968657f085b7add7fc540d40ebc71a3bc
Karel Šťastný <karell29@icloud.com>
2023-06-05 16:15:27 +0200

    Přidány nové stránky pro jednotlivé stránky, natáhnutí přesnách dat a styly

---
+++ script/onebooks.js
@@ -0,0 +1,336 @@
+let allBooks = [ {
+    id: 0,
+    name: "Zaklínač I. Poslední přání",
+    publication: 1999,
+    image:"../images/books/book-1.jpg" ,
+    author: "Andrzej Sapkowski",
+    pages: 280,
+    ISBN: "80-85951-14-2",
+    language: "český", 
+    popis:"Nově přeložené geraltovské povídky ze Stříbrného meče, něco z Věčného ohně a Meče osudu & kompletní povídka Hlas rozumu, kde Geralt vysvětluje, jak to všechno začalo.Zaklínač, ošlehaný muž bez věku, jehož bílé vlasy nejsou znakem stáří, ale mutace, kterou musel podstoupit. Placený i dobrovolný likvidátor prapodivných tvorů: mantichor, trollů, vidlohonů, strig, amfisbain – pokud ovšem ohrožují lidský rod; v takovém případě zabíjí i bytost zvanou člověk. Prvotřídní, skvělý bojovník, který není neporazitelný ani nezranitelný – naopak, téměř z každého dobrodružství si odnáší další šrámy na těle i na duši.",
+    link: "../pages/part_1.html",
+    url: "https://www.knihydobrovsky.cz/kniha/zaklinac-i-posledni-prani-13819",
+},{
+    id: 1,
+    name: "Zaklínač II. Meč Osudu",
+    publication: 2000,
+    image:"../images/books/book-2.jpg" ,
+    author: "Andrzej Sapkowski",
+    pages: 344,
+    ISBN: "80-85951-23-1",
+    language: "český", 
+    popis:"Hlavním hrdinou tvorby A. Sapkowského je zaklínač Geralt z Rivie. Jako nájemný hubitel upírů, vlkodlaků a všemožných nebezpečných netvorů pochopitelně ovládá bojové a magické techniky nezbytné pro jeho zaměstnání. nicméně není pouze chladnokrevným profesionálem a už vůbec ne jedním ze superhrdinů, jimiž se žánr fantasy jen hemží. Postupně vychází najevo, že neměl možnost si svůj osud svobodně zvolit, ale naopak je nucen draze platit za schopnosti a dovednosti nedosažitelné a vesměs též nepochopitelné běžným smrtelníkům. Proto vyvolává strach a zároveň nevraživost a odpor těch, jejichž životy vlastně chrání. Přes své pozitivní působení jsou zaklínači odsouzeni k vyhnanství na samém okraji společnosti, která sice působí vnějškově středověkým dojmem, avšak s mnoha atributy převzatými ze současnosti.",
+    link: "../pages/part_2.html",
+    url: "https://www.databazeknih.cz/knihy/mec-osudu-328737",
+},{
+    id: 2,
+    name: "Zaklínač III. Krev Elfů",
+    publication: 2009,
+    image:"../images/books/book-3.jpg" ,
+    author: "Andrzej Sapkowski",
+    pages: 316,
+    ISBN: "978-80-7477-081-4",
+    language: "český", 
+    popis:"Na stránkách strhujícího fantasy románu, v němž se odehrává boj o osud světa, Zaklínač III. Krev elfů, který napsal Andrzej Sapkowski, se poprvé setkáváme s oblíbenými hrdiny ze Sapkowského povídek. Cintra je v troskách a stala se nilfgaardským protektorátem. Válka hrozí, že zasáhne i všechy okolní země, jež se stanou vazalskými provinciemi pod nadvládou nilfgaardského císaře Emhyra. Jediný, kdo může válečné štěstí vychýlit na jednu či na druhou stranu, je zmizelá princezna Cirilla, vnučka cintránské královny Calanthé. A tak ji hledají snad všichni jedni aby s ní uzavřeli dynastický sňatek, druzí aby něco takového znemožnili. Je na Geraltovi, aby s pomocí svých přátel splnil slib, že malou princezničku ochrání. Úkol je to těžký, vždyť válečnou mašinerii nějaké bajky o Osudu a Dítěti-překvapení nijak zvlášť nezajímají (první román o Geraltovi a Ciri III.)",
+    link: "../pages/part_3.html",
+    url: "https://www.knihydobrovsky.cz/kniha/zaklinac-iii-krev-elfu-11965",
+},{
+    id: 3,
+    name: "Zaklínač IV. Čas Opovržení",
+    publication: 2011,
+    image:"../images/books/book-4.jpg" ,
+    author: "Andrzej Sapkowski",
+    pages: 342,
+    ISBN: "978-80-85951-70-7",
+    language: "český", 
+    popis:"Druhý román o Geraltovi a Ciri. Svět zachvátily plameny běsnící války. Čarodějové intrikují a Ciri, prchající před smrtelným nebezpečím, se ocitá daleko od Geralta i Yennefer. Sama, opuštěná, ztracená - s pocitem, že byla zrazena těmi, kterým bezmezně věřila. Pátrání po zmizelé princezně však navzdory zprávám o její smrti stále pokračuje, a Geralt žel není jediný, kdo Ciri hledá. Zaklínač i Lvíče z Cintry se stali objektem vysoké politické hry, která může zabít nejen je, ale také všechny, kdo jsou jim blízcí. Ostatně, věštba přece tvrdí, že princezně kráčí v patách smrt...",
+    link: "../pages/part_4.html",
+    url: "https://www.knihydobrovsky.cz/kniha/zaklinac-iv-cas-opovrzeni-11972",
+},{
+    id: 4,
+    name: "Zaklínač V. Křest ohněm",
+    publication: 2011,
+    image:"../images/books/book-5.jpg" ,
+    author: "Andrzej Sapkowski",
+    pages: 353,
+    ISBN: "978-80-85951-71-4",
+    language: "český", 
+    popis:"Třetí román o Geraltovi a Ciri. Císař Emhyr chystá svatbu - po sňatku s princeznou Cirillou se stane nejen vládcem Citry, ale podle věštby také pánem celého Severu. Že to s oním plánem nebude tak jednoduché, císař zjistí téměř okamžitě - někdo mu podstrčil dvojnici. Přestože se snaží své odhalení utajit a pokračuje v přípravách sňatku, pravdu nakonec zjistí nejen Dijkstrova tajná služba, ale i společenství čarodějek, jež se rozhodly, že osud světa konečně vezmou do svých rukou. Jen Geralt o podvržené princezně netuší nic. Rozhodne se, že vyrazí do Nilfgaardu a Ciri osvobodí. Aby se však dostal k ústí Jarugy, musí projít územím, na němž zuří válka. A jak se zdá, nemůže si být jist svým životem ani před vojáky útočícího Nilfgaardu, ani před jednotkami obránců, a tím méně před záškodnickými komandy Veverek...",
+    link: "../pages/part_5.html",
+    url: "https://www.knihydobrovsky.cz/kniha/zaklinac-v-krest-ohnem-38398",
+
+},{
+    id: 5,
+    name: "Zaklínač VI. Věž vlaštovky",
+    publication: 2013,
+    image:"../images/books/book-6.jpg" ,
+    author: "Andrzej Sapkowski",
+    pages: 456,
+    ISBN: "978-80-85951-73-8",
+    language: "český", 
+    popis:"Vysogota z Corvia, zneuznaný filozof, který žije ve vyhnanství uprostřed močálů, nachází jednoho dne těžce zraněnou dívku. Ošetří ji a zachrání ji život. Čas plyne a ona mu vypráví svůj příběh, snažíce se zakrýt pramenem vlasů hlubokou jizvu hyzdící její tvář... Poznáváme příběh o tom, jak se z Ciri stal nelítostný zabiják, dívka smrtící ve svém hněvu a neochvějná ve svých rozhodnutích. Poznáváme co se dělo s bandou potkanů a seznamujeme se s nájemným vrahem, jehož jméno jest Bonhart. A setkáváme se se smrtí... Sledujeme putování Geraltovo a hledání Druidů, kteří by mohli vědět, kde se Ciri nachází. Narážíme na půlelfa Schirů, jehož úkolem je Geralta zabít. Sledujeme plameny a zakrýváme si uši před výkřiky upalovaných... Posloucháme, jak elf Avallac'h rozpráví o tajemném proroctví pronásledujícím Ciri, následujeme kroky Yennefer při hledání úkrytu mága Vilgefortze. Účastníme se soudu s vnímačkou Kennou. Dozvídáme se, jak Ciri přišla ke svému meči a koni... A na každém kroku je smrt, utrpení. Osud je nevyhnutelný a vše spěje k jednomu jedinému bodu. K jakému? Kdo ví... Nikdo si nemůže být jist svým životem, nikomu se nedá věřit. Přišel věk meče a sekyry, věk vlčí...",
+    link: "../pages/part_6.html",
+    url: "https://www.knihydobrovsky.cz/kniha/zaklinac-vi-vez-vlastovky-37614",
+
+},{
+    id: 6,
+    name: "Zaklínač VII. Paní jezera",
+    publication: 2013,
+    image:"../images/books/book-7.jpg" ,
+    author: "Andrzej Sapkowski",
+    pages: 559,
+    ISBN: "978-80-7477-086-9",
+    language: "český", 
+    popis:"Had Uroboros se zakousl do vlastního ocasu. Něco končí, něco začíná. Také příběh Zaklínačova hledání dospěl k závěru, Ciri, Geralt i Yennefer se na pozadí krvavého válečného konfliktu konečně setkají, ovšem podobně jako v předchozích dílech ságy nelze očekávat, že věci půjdou tak snadno, jak byly naplánovány. Příběh Dítěte Překvapení, zlověstné věštby, lásky, krutosti a událostí, měnících tvář světa, se uzavírá v mohutném finále, v gejzíru imaginace, aby jednou provždy změnil fantastickou literaturu. Had Uroboros drží v zubech vlastní ocas, něco skončilo a cosi začíná.",
+    link: "../pages/part_7.html",
+    url: "https://www.knihydobrovsky.cz/kniha/zaklinac-vii-pani-jezera-90612",
+
+},]
+
+
+// Funkce pro výpis knih na každou stránku
+let oneBook = () => {
+    // číslo knih
+
+    let number = ""
+
+    // podle page zadej číslo 
+    if(document.querySelector(".part_01")){
+        number = allBooks[0].id
+    } else if(document.querySelector(".part_02")){
+        number =  allBooks[1].id
+    } else if(document.querySelector(".part_03")){
+        number =  allBooks[2].id
+    } else if(document.querySelector(".part_04")){
+        number =  allBooks[3].id
+    } else if(document.querySelector(".part_05")){
+        number = allBooks[4].id
+    } else if(document.querySelector(".part_06")){
+        number =  allBooks[5].id
+    } else if(document.querySelector(".part_07")){
+        number =  allBooks[6].id
+    }
+
+
+    // Vytvoření html struktury
+    let part = document.createElement("div")
+    part.innerHTML = `
+    
+    <div class="containerPart">
+               <div class="box">
+                   <img src="${allBooks[number].image}">
+                   <div class="details">
+                       <h3>${allBooks[number].name}</h3>
+                       <p>Publikováno:${allBooks[number].publication}</p>
+                       <p>Autor:${allBooks[number].author}</p>
+                       <p>Počet Stran:${allBooks[number].pages}</p>
+                       <p>ISBN:${allBooks[number].ISBN}</p>
+                       <p>Jazyk:${allBooks[number].language}</p>
+                       <a href="${allBooks[number].url}" target="_blank" rel="noopener noreferrer">Odkaz do obchodu</a>
+                   </div>
+               </div>
+               <div class="description">
+                   <p>${allBooks[number].popis}</p>
+               </div>
+           </div>
+   `
+    document.querySelector(".part").appendChild(part)
+
+}
+
+oneBook()
+
+
+
+
+
+
+
+// if(document.querySelector(".part_01")){
+// let part1 = document.createElement("div")
+//  part1.innerHTML = `
+ 
+//  <div class="containerPart">
+//             <div class="box">
+//                 <img src="${allBooks[0].image}">
+//                 <div class="details">
+//                     <h3>${allBooks[0].name}</h3>
+//                     <p>Publikováno:${allBooks[0].publication}</p>
+//                     <p>Autor:${allBooks[0].author}</p>
+//                     <p>Počet Stran:${allBooks[0].pages}</p>
+//                     <p>ISBN:${allBooks[0].ISBN}</p>
+//                     <p>Jazyk:${allBooks[0].language}</p>
+//                     <a href="${allBooks[0].url}" target="_blank" rel="noopener noreferrer">Odkaz do obchodu</a>
+//                 </div>
+//             </div>
+//             <div class="description">
+//                 <p>${allBooks[0].popis}</p>
+//             </div>
+//         </div>
+// `
+//  document.querySelector(".part_01").appendChild(part1)
+// }
+
+
+//  // Part_2
+// if(document.querySelector(".part_02")){
+//     let part2 = document.createElement("div")
+//     part2.innerHTML = `
+    
+//     <div class="containerPart">
+//                <div class="box">
+//                    <img src="${allBooks[1].image}">
+//                    <div class="details">
+//                        <h3>${allBooks[1].name}</h3>
+//                        <p>Publikováno:${allBooks[1].publication}</p>
+//                        <p>Autor:${allBooks[1].author}</p>
+//                        <p>Počet Stran:${allBooks[1].pages}</p>
+//                        <p>ISBN:${allBooks[1].ISBN}</p>
+//                        <p>Jazyk:${allBooks[1].language}</p>
+//                        <a href="${allBooks[1].url}" target="_blank" rel="noopener noreferrer">Odkaz do obchodu</a>
+//                    </div>
+//                </div>
+//                <div class="description">
+//                    <p>${allBooks[1].popis}</p>
+//                </div>
+//            </div>
+//     `
+//     document.querySelector(".part_02").appendChild(part2)
+// }
+
+
+
+
+// // Part3
+// if(document.querySelector(".part_03")){
+// let part3 = document.createElement("div")
+//  part3.innerHTML = `
+ 
+//  <div class="containerPart">
+//             <div class="box">
+//                 <img src="${allBooks[2].image}">
+//                 <div class="details">
+//                     <h3>${allBooks[2].name}</h3>
+//                     <p>Publikováno:${allBooks[2].publication}</p>
+//                     <p>Autor:${allBooks[2].author}</p>
+//                     <p>Počet Stran:${allBooks[2].pages}</p>
+//                     <p>ISBN:${allBooks[2].ISBN}</p>
+//                     <p>Jazyk:${allBooks[2].language}</p>
+//                     <a href="${allBooks[2].url}" target="_blank" rel="noopener noreferrer">Odkaz do obchodu</a>
+//                 </div>
+//             </div>
+//             <div class="description">
+//                 <p>${allBooks[2].popis}</p>
+//             </div>
+//         </div>
+// `
+//  document.querySelector(".part_03").appendChild(part3)
+// }
+
+// // Part4
+// if(document.querySelector(".part_01")){
+// let part4 = document.createElement("div")
+// part4.innerHTML = `
+
+// <div class="containerPart">
+//            <div class="box">
+//                <img src="${allBooks[0].image}">
+//                <div class="details">
+//                    <h3>${allBooks[0].name}</h3>
+//                    <p>Publikováno:${allBooks[0].publication}</p>
+//                    <p>Autor:${allBooks[0].author}</p>
+//                    <p>Počet Stran:${allBooks[0].pages}</p>
+//                    <p>ISBN:${allBooks[0].ISBN}</p>
+//                    <p>Jazyk:${allBooks[0].language}</p>
+//                    <a href="${allBooks[0].url}" target="_blank" rel="noopener noreferrer">Odkaz do obchodu</a>
+//                </div>
+//            </div>
+//            <div class="description">
+//                <p>${allBooks[0].popis}</p>
+//            </div>
+//        </div>
+// `
+// document.querySelector(".part_04").appendChild(part4)
+// }
+
+// // Part5
+// if(document.querySelector(".part_01")){
+//     let part5 = document.createElement("div")
+//      part5.innerHTML = `
+     
+//      <div class="containerPart">
+//                 <div class="box">
+//                     <img src="${allBooks[0].image}">
+//                     <div class="details">
+//                         <h3>${allBooks[0].name}</h3>
+//                         <p>Publikováno:${allBooks[0].publication}</p>
+//                         <p>Autor:${allBooks[0].author}</p>
+//                         <p>Počet Stran:${allBooks[0].pages}</p>
+//                         <p>ISBN:${allBooks[0].ISBN}</p>
+//                         <p>Jazyk:${allBooks[0].language}</p>
+//                         <a href="${allBooks[0].url}" target="_blank" rel="noopener noreferrer">Odkaz do obchodu</a>
+//                     </div>
+//                 </div>
+//                 <div class="description">
+//                     <p>${allBooks[0].popis}</p>
+//                 </div>
+//             </div>
+//     `
+//      document.querySelector(".part_05").appendChild(part5)
+// }
+
+// // Part6
+// if(document.querySelector(".part_01")){
+//     let part6 = document.createElement("div")
+//     part6.innerHTML = `
+    
+//     <div class="containerPart">
+//                <div class="box">
+//                    <img src="${allBooks[0].image}">
+//                    <div class="details">
+//                        <h3>${allBooks[0].name}</h3>
+//                        <p>Publikováno:${allBooks[0].publication}</p>
+//                        <p>Autor:${allBooks[0].author}</p>
+//                        <p>Počet Stran:${allBooks[0].pages}</p>
+//                        <p>ISBN:${allBooks[0].ISBN}</p>
+//                        <p>Jazyk:${allBooks[0].language}</p>
+//                        <a href="${allBooks[0].url}" target="_blank" rel="noopener noreferrer">Odkaz do obchodu</a>
+//                    </div>
+//                </div>
+//                <div class="description">
+//                    <p>${allBooks[0].popis}</p>
+//                </div>
+//            </div>
+//     `
+//     document.querySelector(".part_06").appendChild(part6)
+    
+// }
+
+// // Part7
+// if(document.querySelector(".part_01")){
+//     let part7 = document.createElement("div")
+//      part7.innerHTML = `
+     
+//      <div class="containerPart">
+//                 <div class="box">
+//                     <img src="${allBooks[0].image}">
+//                     <div class="details">
+//                         <h3>${allBooks[0].name}</h3>
+//                         <p>Publikováno:${allBooks[0].publication}</p>
+//                         <p>Autor:${allBooks[0].author}</p>
+//                         <p>Počet Stran:${allBooks[0].pages}</p>
+//                         <p>ISBN:${allBooks[0].ISBN}</p>
+//                         <p>Jazyk:${allBooks[0].language}</p>
+//                         <a href="${allBooks[0].url}" target="_blank" rel="noopener noreferrer">Odkaz do obchodu</a>
+//                     </div>
+//                 </div>
+//                 <div class="description">
+//                     <p>${allBooks[0].popis}</p>
+//                 </div>
+//             </div>
+//     `
+//      document.querySelector(".part_07").appendChild(part7)
+    
+// }
+
+
