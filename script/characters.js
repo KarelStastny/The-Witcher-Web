9afef7926af7c2dddc44f6b84294c0c9766a2723
Karel Šťastný <karell29@icloud.com>
2023-06-06 10:53:06 +0200

    Vytvořen Základní filter -je potřeba ho doladit

--- script/characters.js
+++ script/characters.js
@@ -12,11 +12,78 @@ const characters = [{
     image: "../images/character/avallach.png"
 },{
     id: 4,
-    name: "Barnabas.png",
+    name: "Barnabas",
     image: "../images/character/barnabas.png"
 },{
     id: 5,
-    name: "Bart.png",
+    name: "Bart",
     image: "../images/character/bart.png"
 },
 ]
+
+
+
+// Filtrování všech Charakterů
+const search = document.querySelector(".search-character")
+const listCharacter = document.querySelector(".character-list")
+
+
+// Vyhledávací políčko
+search.addEventListener("input", (e) => {
+    // Na text v políčku zavolána vyhledávací funkce
+    charactersFilter(e.target.value)
+})
+
+
+
+// Pole pro všechny charaktery
+let allCharacter = []
+
+
+
+// Výpis všech dat
+characters.map( (oneCharacter) =>{
+    let card = document.createElement("div")
+
+
+    
+
+
+    // Vytvoření struktury
+    card.innerHTML = `
+    <div class="card">
+    <img src="${oneCharacter.image}" alt="${oneCharacter.name}">
+    <p>${oneCharacter.name}</p>
+    </div>`
+    console.log(oneCharacter);
+
+
+    // Přidání do stránky a do nového pole
+    listCharacter.appendChild(card)
+    allCharacter.push(card)
+
+   
+
+})
+
+
+
+
+// Filtrování pomocí přidání classy hide + CSS
+charactersFilter = (inputText) => {
+    allCharacter.forEach( (oneCharacter) => {
+        if(oneCharacter.innerHTML.toLowerCase().includes(inputText.toLowerCase())){
+            oneCharacter.classList.remove("hide")
+        }else{
+            oneCharacter.classList.add("hide")
+        }
+    })
+}
+
+
+
+
+
+
+
+
