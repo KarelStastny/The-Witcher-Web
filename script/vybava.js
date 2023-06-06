06bea73b1c5f6f843c8ac35806eff87941f98b9e
Karel Šťastný <karell29@icloud.com>
2023-06-06 14:53:30 +0200

    vytvořena základní databáze výbavy

---
+++ script/vybava.js
@@ -0,0 +1,32 @@
+const vybava = [{
+    id: 1,
+    name: "Addan Deith", 
+    category: "sword",
+    image:"../images/vybava/addan_deith.png"
+},{
+    id: 2,
+    name: "Alder Folk", 
+    category: "boots",
+    image:"../images/vybava/Alder_Folk_boots.png"
+},{
+    id: 3,
+    name: "Angrenian", 
+    category: "armor",
+    image:"../images/vybava/Angrenian_cuirass.png"
+},{
+    id: 4,
+    name: "Asassins Gauntents", 
+    category: "gloves",
+    image:"../images/vybava/assassin_gauntlets.png"
+},{
+    id: 5,
+    name: "Axemans Trousers", 
+    category: "trousers",
+    image:"../images/vybava/axemans_trousers.png"
+},{
+    id: 6,
+    name: "Crossbow", 
+    category: "crossbow",
+    image:"../images/vybava/crossbow.png"
+},
+]
