befc84bdeba471794fda720cfc1841db174b2d00
Karel Šťastný <karell29@icloud.com>
2023-06-06 12:53:26 +0200

    Přidána stránka monstra a vytvořní základní databáze

---
+++ script/monsters.js
@@ -0,0 +1,16 @@
+const monsters = [{
+    id: 1,
+    name: "Alghoul",
+    image: "../images/monsters/alghoul.png",
+},{
+    id: 2,
+    name: "Arachas",
+    image: "../images/monsters/arachas.png",
+},{
+    id: 3,
+    name: "Armor Edarachas",
+    image: "../images/monsters/armoredarachas.png",
+},
+]
+
+
