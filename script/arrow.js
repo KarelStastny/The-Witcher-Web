2adfbc84375f229a39c097a23b9583bddcee58b5
Karel Šťastný <karell29@icloud.com>
2023-06-15 08:38:14 +0200

    Uklizeny nepotřebné věci v HTML a SCSS

--- script/arrow.js
+++ script/arrow.js
@@ -12,19 +12,4 @@ window.addEventListener("scroll", () => {
 })
 
 
-// Progress bar
 
-function updateProgressBar (){
-    // Získání scrollTop a scrollHeight
-    const {scrollTop, scrollHeight} = document.documentElement
-  
-    // Výpočet pro zisk v % aktuální výšky
-    const scrollPercent = `${(scrollTop / (scrollHeight - window.innerHeight)) *100}%`
-
-    // aktualizace v CSS 
-    document.querySelector("#progress-bar").style.width = scrollPercent
-    
-}
-
-//Scroll Progress
-document.addEventListener("scroll", updateProgressBar)
